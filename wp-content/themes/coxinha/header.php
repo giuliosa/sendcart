9217cbd6f1d7e9cac6e72c274902504d91369b18
giuliosa <giuliocaetano@yahoo.com.br>
2018-01-17 16:29:54 -0300

    Adicionando header e index para elementor

--- wp-content/themes/coxinha/header.php
+++ wp-content/themes/coxinha/header.php
@@ -6,11 +6,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
+    <meta name="viewport" content="width=device-width">
+    <title>SendCart - Importação de Produtos</title>
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
     <?php wp_head(); ?>
 
 
-    <meta name="viewport" content="width=device-width">
 </head>
 <body <?php body_class(); ?>>
         
--- wp-content/themes/coxinha/index.php
+++ wp-content/themes/coxinha/index.php
@@ -1,9 +1,6 @@
 <?php get_header(); ?>
 
-	<h1>Welcome to Olympos theme.</h1>
-    <p>
-        You can delete this text in index.php
-    </p>
+<?php the_content(); ?>
 
     
 <?php get_footer(); ?>
